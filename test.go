7c0bdcc4c2386c506eab8e9966a467cdf2770aad
Name <ffff@exf.com>
2020-08-06 15:31:42 +0300

    preview

--- test.go
+++ test.go
@@ -2,21 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"time"
-
-	//"gopkg.in/mgo.v2/bson"
-
-	//"go.mongodb.org/mongo-driver/mongo/readpref"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"os"
-	//"net/http"
-	"github.com/dgrijalva/jwt-go"
-	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
 type User struct {
@@ -48,39 +44,39 @@ func generateKeys(user string) (string, []string){
 func setCookies(user, ac, rc string, writer http.ResponseWriter){
 	nameCookie := http.Cookie{Name: "username", Value: user, Secure: false}
 	authCookie := http.Cookie{Name: "ac", Value: ac, Secure: false}
-	refCookie := http.Cookie{Name: "rc", Value: rc, Secure: false}
+	refCookie := http.Cookie{Name: "rc", Value: base64.StdEncoding.EncodeToString([]byte(rc)), Secure: false}
 
 	http.SetCookie(writer, &nameCookie)
 	http.SetCookie(writer, &authCookie)
 	http.SetCookie(writer, &refCookie)
 }
 
+func getConnection() *mongo.Collection{
+	client, _ := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://user31:1337228@cluster0.tw4ir.mongodb.net/users?retryWrites=true&w=majority\n"))
+	client.Connect(context.TODO())
+	collection:= client.Database("users").Collection("keys")
+
+	return collection
+}
+
+func decodeRefToken(str string) string {
+	refKeyDecode, _:=base64.StdEncoding.DecodeString(str)
+	refKeyDecodeString:=string(refKeyDecode)
+
+	return refKeyDecodeString
+}
+
 func main() {
 	http.HandleFunc("/getTokens", func(writer http.ResponseWriter, request *http.Request) {
 		user:= request.URL.Query().Get("login")
 
-		token:= jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{ "Login": user, "Key":  time.Now().Unix()})
-		tokenString, _:= token.SignedString([]byte(os.Getenv("KEY")))
-		key, _ := bcrypt.GenerateFromPassword([]byte(tokenString), 4)
-
-		client, _ := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
-		client.Connect(context.TODO())
-		collection:= client.Database("users").Collection("keys")
-
-		strings := []string{string(key)}
-		p1 := &User{Id: primitive.NewObjectID(), Login: user, AuthKey: tokenString, RefKeys: strings}
-
+		tokenString, refKey := generateKeys(user)
+		collection:=getConnection()
+		p1 := &User{Id: primitive.NewObjectID(), Login: user, AuthKey: tokenString, RefKeys: refKey}
 		collection.InsertOne(context.TODO(), p1)
+		setCookies(user, tokenString, refKey[0], writer)
 
-		nameCookie := http.Cookie{Name: "username", Value: user, Secure: false}
-		authCookie := http.Cookie{Name: "ac", Value: tokenString, Secure: false}
-		refCookie := http.Cookie{Name: "rc", Value: string(key), Secure: false}
-
-		http.SetCookie(writer, &nameCookie)
-		http.SetCookie(writer, &authCookie)
-		http.SetCookie(writer, &refCookie)
-
-		fmt.Fprintf(writer, string(key))
+		fmt.Fprintf(writer, refKey[0])
 
 	})
 	http.HandleFunc("/refresh", func(writer http.ResponseWriter, request *http.Request) {
@@ -88,16 +84,15 @@ func main() {
 		authToken, _ := request.Cookie("ac")
 		refToken, _ := request.Cookie("rc")
 
-		client, _ := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
-		client.Connect(context.TODO())
-		collection:= client.Database("users").Collection("keys")
+		collection:=getConnection()
 
-		filter := bson.M{ "login": login.Value } // , bson.M{ "$in": []string {refToken.Value} } , "authKey": authToken.Value
+		filter := bson.M{ "login": login.Value }
 
 		var user User
 		collection.FindOne(context.TODO(), filter).Decode(&user)
+		refKeyDecodeString:=decodeRefToken(refToken.Value)
 
-		if user.AuthKey == authToken.Value && stringInSlice(refToken.Value, user.RefKeys) {
+		if user.AuthKey == authToken.Value && stringInSlice(refKeyDecodeString, user.RefKeys) {
 			at, rt := generateKeys(login.Value)
 			collection.UpdateOne(context.TODO(), bson.M{ "login": login.Value }, bson.M{"$set": bson.M{"authkey": at, "refkeys": rt}})
 			setCookies(login.Value, at, rt[0], writer)
@@ -112,43 +107,19 @@ func main() {
 		} else {
 			fmt.Fprintf(writer, "Такого пользователя нет!")
 		}
-
-		//fmt.Fprintf(writer, user.Login)
 	})
 	http.HandleFunc("/delToken", func(writer http.ResponseWriter, request *http.Request) {
-		tokenString:= request.URL.Query().Get("rc")
-		client, _ := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
-		client.Connect(context.TODO())
-		collection:= client.Database("users").Collection("keys")
+		token:= request.URL.Query().Get("rc")
+		tokenString:=decodeRefToken(token)
+		collection:=getConnection()
 
 		collection.UpdateOne(context.TODO(), bson.M{"refkeys" : bson.M{"$eq": tokenString}}, bson.M{"$pull" : bson.M{"refkeys" :  tokenString }})
-		//fmt.Fprint(writer, int(res.ModifiedCount), err)res,err:=
 	})
 	http.HandleFunc("/delAllTokens", func(writer http.ResponseWriter, request *http.Request) {
 		login:= request.URL.Query().Get("login")
-		client, _ := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
-		client.Connect(context.TODO())
-		collection:= client.Database("users").Collection("keys")
+		collection:=getConnection()
 
 		collection.UpdateOne(context.TODO(), bson.M{"login" : login}, bson.M{"$unset" : bson.M{"refkeys" :  "" }})
 	})
 	http.ListenAndServe(":80", nil)
-	/*
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		cookie := http.Cookie{Name: "username", Value: "astaxie", Secure: false}
-		http.SetCookie(writer, &cookie)
-		cookie1, _ := request.Cookie("username")
-		fmt.Fprint(writer, cookie1)
-	})
-	http.ListenAndServe(":80", nil)
-	*/
-	token:= jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{ "Hell": "No",
-																		"No": "Hell",})
-	tokenString, err:= token.SignedString([]byte(os.Getenv("1337")))
-	fmt.Println(tokenString, err)
-
-	result, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error){ return []byte(os.Getenv("1337")), nil})
-	claims := result.Claims.(jwt.MapClaims)
-	fmt.Println(claims["Hell"])
 }
-
